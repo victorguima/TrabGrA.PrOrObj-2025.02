fc9b8859a235843c900ca9759303dd95bce8c978
Victor <43277299+victorguima@users.noreply.github.com>
2025-09-12 15:07:24 -0300

    Caso 7 finalizado
    Agora também exibe as informações lidas sobre os clientes e os produtos que podem ser registrados.
    
    Exibe mensagens de erro apropriadas.
    
    Adicionados os métodos da classe Quarto que estavam faltando, e já testa eles no final do código.

--- src/App.java
+++ src/App.java
@@ -32,27 +32,54 @@ public class App {
 
                     break;
                 case 7:
-                    System.out.println("Clientes com reservas ativas:");
+                    System.out.println("Clientes que já fizeram check-in:");
                     for(Reserva r : pousada.getReservas()){
-                        if(r.getStatus()=='A') //Mostra apenas reservas ativas
-                            System.out.println(r.getCliente());
+                        if(r.getStatus()=='I') //Mostra apenas reservas com check-in realizado
+                            System.out.println(r.getCliente()+" - Quarto "+r.getQuarto().getNumero() + ", de dias "+r.getDiaInicio()+" a "+r.getDiaFim());
                     }
                     System.out.println("Digite o nome do cliente:");
                     String cliente = scanner.nextLine();
                     boolean clienteEncontrado = false;
                     for(Reserva r: pousada.getReservas()){
-                        if(r.getCliente().equals(cliente)){
-                            for(Produto p: pousada.getProdutos()){ //Mostra a lista de produtos disponíveis
-                                System.out.println(p.toString());
+                        if(r.getCliente().equals(cliente)){ //Checa se o nome do cliente bate com alguma reserva
+                            switch (r.getStatus()) {
+                                case 'I':
+                                    for(Produto p: pousada.getProdutos()){ //Mostra a lista de produtos disponíveis
+                                        System.out.println(p.toString());
+                                    }
+                                    System.out.println("Digite o código do produto consumido, 0 para sair:");
+                                    int codProduto = scanner.nextInt();
+                                    boolean produtoValido = false;
+                                    do{
+                                        for(Produto p: pousada.getProdutos()){
+                                            if(p.getCodigo()==codProduto){
+                                                r.getQuarto().adicionaConsumo(codProduto);
+                                                System.out.println(p.getNome()+" - R$ "+p.getPreco()+" registrado com sucessoo.");
+                                                produtoValido = true;
+                                            }
+                                        }
+                                        if(!produtoValido && codProduto!=0){
+                                            System.out.println("Código inválido, tente novamente.");
+                                        }
+                                        produtoValido = false; //reinicia a variável para a próxima iteração
+                                    }while((codProduto=scanner.nextInt())!=0); //Lê os códigos dos produtos consumidos até o usuário digitar 0
+                                    break;
+                                case 'O':
+                                    System.out.println("O cliente já realizou check-out.");
+                                    break;
+                                case 'A':
+                                    System.out.println("O cliente ainda não realizou check-in.");
+                                    break;
+                                case 'C':
+                                    System.out.println("O cliente cancelou sua reserva.");
+                                    break;
+                                default:
+                                    break;
                             }
-                            System.out.println("Digite o código do produto consumido:");
-                            int codProduto = scanner.nextInt();
-                            r.getQuarto().adicionaConsumo(codProduto);
-                            System.out.println("Consumo registrado com sucesso!");
                             clienteEncontrado = true;
                         }
                     }
-                    if(!clienteEncontrado){
+                    if(!clienteEncontrado){ //Mostra a mensagem apenas uma vez
                         System.out.println("Cliente não encontrado.");
                     }
                     break;
@@ -62,8 +89,13 @@ public class App {
             }
         } while(codigo != 0);
         scanner.close();
-        for(Quarto q: pousada.getQuartos()){
+        for(Quarto q: pousada.getQuartos()){ //Mostra o consumo de todos os quartos
             System.out.println(q.toString());
+            if(q.getConsumo().length>0){//Se o quarto tiver consumos registrados
+                System.out.println("Consumo do quarto "+q.getNumero()+":");
+                q.listaConsumo(pousada);
+                q.valorTotalConsumo(pousada);
+            }
         }
     }
     
